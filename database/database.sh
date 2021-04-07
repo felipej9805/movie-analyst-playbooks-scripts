a31ee23402450d8c9cdb341be7f1382c65ec8c94
Felipe <felipej9805@gmail.com>
2021-04-07 15:37:38 -0500

    Delete specific yml files and update general yml file

--- database/database.sh
+++ database/database.sh
@@ -1,3 +1,6 @@
+echo 'Export variables from .env file...'
+set -o allexport; . ~/.env; set +o allexport
+
 echo 'Read variable host ...'
 env_path=$1
 container_name="database_host"
@@ -9,7 +12,7 @@ sudo docker volume create db-data
 echo 'Starting run database Container...'
 echo '################################################################################'
 sudo docker rm -f $container_name > /dev/null 2>&1
-docker run -d -p 3306:3306 --env-file $env_path --name $container_name --mount source=db-data,target=/var/lib/mysql 9805/mydatabase:v1
+sudo docker run -d -p 3306:3306 --name $container_name --mount source=db-data,target=/var/lib/mysql 9805/mydatabase:v1
 
 echo 'Adding script to automatic restart containers...'
 echo '################################################################################'
