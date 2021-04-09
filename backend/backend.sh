26122ab08341a45bd20d9a34b1e7dcac00da57c5
Felipe <felipej9805@gmail.com>
2021-04-09 08:05:37 -0500

    Create backend.sh 1 version

---
+++ backend/backend.sh
@@ -0,0 +1,22 @@
+echo 'Export variables from .env file...'
+set -o allexport; . ~/.env; set +o allexport
+
+echo 'Read variable host ...'
+env_path=$1
+container_name="backend_host"
+
+echo 'Starting run backend Container...'
+echo '################################################################################'
+sudo docker rm -f $container_name > /dev/null 2>&1
+
+sudo docker run -d -p 3000:3000 --name $container_name 9805/mybackend:v1
+
+echo 'Adding script to automatic restart containers...'
+echo '################################################################################'
+sudo install -m 777 /dev/null /etc/rc.local
+sudo echo -e '#!/bin/bash\nsudo docker restart $(sudo docker ps -aq)\nexit 0' > /etc/rc.local
+
+echo '################################################################################'
+echo 'Backend container is ready... :)'
+echo '################################################################################'
+
