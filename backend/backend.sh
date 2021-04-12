540da83cb0bd4cccb2667a0461045897314250d9
Felipe <felipej9805@gmail.com>
2021-04-12 11:24:29 -0500

    Change script to build the image with arg

--- backend/backend.sh
+++ backend/backend.sh
@@ -6,11 +6,18 @@ echo 'Read variable host ...'
 env_path=$1
 container_name="backend_host"
 
+echo 'Building image...'
+docker build --build-arg PORT \
+--build-arg DB_HOST \
+--build-arg DB_USER \
+--build-arg DB_PASS \
+--build-arg DB_NAME -t 9805/mybackend /home/ubuntu/movie-analyst-api/Dockerfile
+
 echo 'Starting run backend Container...'
 echo '################################################################################'
 sudo docker rm -f $container_name > /dev/null 2>&1
 
-sudo docker run -d -p 3000:3000 --name $container_name 9805/mybackend:v1
+sudo docker run -d -p 3000:3000 --name $container_name 9805/mybackend
 
 echo 'Adding script to automatic restart containers...'
 echo '################################################################################'
--- database/database.sh
+++ database/database.sh
@@ -1,5 +1,10 @@
 echo 'Export variables from .env file...'
 set -o allexport; . ~/.env; set +o allexport
+. /home/ubuntu/.bashrc
+. /home/ubuntu/.profile
+
+echo 'Building image...'
+docker build --build-arg MYSQL_ROOT_PASSWORD -t 9805/mydatabase /home/ubuntu/movie-analyst-api/data_model/Dockerfile
 
 echo 'Read variable host ...'
 env_path=$1
@@ -12,7 +17,7 @@ sudo docker volume create db-data
 echo 'Starting run database Container...'
 echo '################################################################################'
 sudo docker rm -f $container_name > /dev/null 2>&1
-sudo docker run -d -p 3306:3306 --name $container_name --mount source=db-data,target=/var/lib/mysql 9805/mydatabase:v1
+sudo docker run -d -p 3306:3306 --name $container_name --mount source=db-data,target=/var/lib/mysql 9805/mydatabase
 
 echo 'Adding script to automatic restart containers...'
 echo '################################################################################'
